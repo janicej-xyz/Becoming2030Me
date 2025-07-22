e4c8fa911ae000c8b2cf12b1faf830c594a72472
Janice Jiang <jiaojiang@squareup.com>
2025-07-22 16:49:33 -0700

    Initial Commit

---
+++ Becoming2030Me/Becoming2030MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Becoming2030MeApp.swift
+//  Becoming2030Me
+//
+//  Created by Janice Jiang on 7/22/25.
+//
+
+import SwiftUI
+
+@main
+struct Becoming2030MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Becoming2030MeTests/Becoming2030MeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Becoming2030MeTests.swift
+//  Becoming2030MeTests
+//
+//  Created by Janice Jiang on 7/22/25.
+//
+
+import Testing
+@testable import Becoming2030Me
+
+struct Becoming2030MeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
